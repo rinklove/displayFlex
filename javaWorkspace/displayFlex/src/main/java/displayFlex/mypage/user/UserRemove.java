80528e6001efaeefda1fe5cd480d9a22e3d82717
WOO\edc92 <dldncjfdla@naver.com>
2023-12-01 14:15:35 +0900

    마이페이지 작성중...

--- javaWorkspace/displayFlex/src/main/java/displayFlex/mypage/user/UserRemove.java
+++ javaWorkspace/displayFlex/src/main/java/displayFlex/mypage/user/UserRemove.java
@@ -1,5 +1,20 @@
 package displayFlex.mypage.user;
 
-public class UserRemove {
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/mypage/userRemove")
+public class UserRemove extends HttpServlet {
+	
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.getRequestDispatcher("/WEB-INF/views/mypage/userRemove.jsp").forward(req, resp);
+		
+	}
 
 }
