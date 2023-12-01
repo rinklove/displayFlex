e7256bd7aec930d806ddf6f6e8f204a4bb8433ac
rinklove <88640680+rinklove@users.noreply.github.com>
2023-12-01 11:15:31 +0900

    fix: 상영 정보 시간 조건 선택 코드 안정화

--- javaWorkspace/displayFlex/src/main/webapp/resources/js/screening-info/add.js
+++ javaWorkspace/displayFlex/src/main/webapp/resources/js/screening-info/add.js
@@ -76,10 +76,9 @@
   
   window.onload = () => {
 	  
-	  document.getElementById("startTime").addEventListener("change", () => {
+	  document.getElementById("startTime").addEventListener("input", () => {
 		  const startTime = document.getElementById("startTime").value;
 		 flatpickr('#endTime', {
-		  value:'',
 	      enableTime: true,       // 시간 선택 활성화
 	      minTime: startTime, 
 	      noCalendar: true,       // 달력 비활성화
@@ -88,7 +87,24 @@
 	      locale: 'ko', // 한국어로 설정
 	    });
 	  });
-
+	
+	
+	document.getElementById("startTime").addEventListener("change", () => {
+		  const startTime = document.getElementById("startTime");
+		  const endTime = document.getElementById("endTime");
+		  if(startTime.value > endTime.value) {
+			 flatpickr('#endTime', {
+			  value: '',
+		      enableTime: true,       // 시간 선택 활성화
+		      minTime: startTime.value, 
+		      noCalendar: true,       // 달력 비활성화
+		      dateFormat: "H:i",      // 시간 형식 설정
+		      time_24hr: true,        // 24시간 형식 사용
+		      locale: 'ko', // 한국어로 설정
+		    });
+			  
+		  } 
+	  });
   }
   
   
