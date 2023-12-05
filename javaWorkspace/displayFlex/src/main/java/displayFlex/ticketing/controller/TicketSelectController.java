ead202a079192d6649120dc57a7d90e2d35041bb
JB <whdqja918@gmail.com>
2023-12-05 17:55:46 +0900

    2023-12-05 영화예매 아무것도 안했는데 푸시하래서 함

--- javaWorkspace/displayFlex/src/main/java/displayFlex/ticketing/controller/TicketSelectController.java
+++ javaWorkspace/displayFlex/src/main/java/displayFlex/ticketing/controller/TicketSelectController.java
@@ -1,6 +1,7 @@
 package displayFlex.ticketing.controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -19,9 +20,18 @@ public class TicketSelectController extends HttpServlet{
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		
+		req.setCharacterEncoding("UTF-8");
+		resp.setContentType("application/json;charset=UTF-8");
+		resp.setCharacterEncoding("UTF-8");
+		
 		try {
 			TicketSelectService tss = new TicketSelectService();
 			List<SelectMovieVo> movieList = tss.getMovieList();
+			PrintWriter out = resp.getWriter();
+			String x = "{\"no\":\"11111111\",\"name\":\"222222222\"}";
+			out.write(x);
+			
+//			req.setAttribute("screenDateList", screenDateList);
 			req.setAttribute("movieList", movieList);
 			req.getRequestDispatcher("/WEB-INF/views/ticketing/ticketSelect.jsp").forward(req, resp);
 		} catch(Exception e) {
