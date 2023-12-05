f5e13db303404493f39aca8ad8d855759c6ac208
WOO\edc92 <dldncjfdla@naver.com>
2023-12-05 17:52:25 +0900

    회원가입 추가!!

--- javaWorkspace/displayFlex/src/main/java/displayFlex/member/MemberService.java
+++ javaWorkspace/displayFlex/src/main/java/displayFlex/member/MemberService.java
@@ -20,4 +20,45 @@ public class MemberService {
 		return loginMember;
 	}
 
+	public int join(MemberVo vo) throws Exception {
+		
+		//conn
+		Connection conn = JDBCTemplate.getConnection();
+		
+//		//busniesss logic
+//		//아이디 4~12 영문소문자 + 숫자
+//		String id = vo.getMemberId();
+//		boolean idOk = id.matches("[a-z0-9]{4,12}");
+//		
+//		if(!idOk) {
+//			throw new Exception("아이디 길이가 맞지 않습니다.");
+//		}
+//		//비밀번호 일치여부체크
+//		if(!vo.getMemberPwd().equals(vo.getMemberPwd())) {
+//			throw new Exception("비밀번호가 일치하지 않습니다.");
+//		}
+//		//비밀번호 4글자 이상
+//		if(vo.getMemberPwd().length() < 4) {
+//			throw new Exception("비밀번호는 4글자 이상 써주세요.");
+//		}
+//		//닉네임에 admin 금지
+//		if(vo.getMemberNick().contains("관리자") || vo.getMemberNick().toLowerCase().contains("admin") || vo.getMemberNick().contains("ADMIN")) {
+//			throw new Exception("관리자는 안됩니다.");
+//		}
+		
+		//dao
+		MemberDao dao = new MemberDao();
+		int result = dao.join(conn, vo);
+		
+		//tx
+		if(result == 1) {
+			JDBCTemplate.commit(conn);
+		}else {
+			JDBCTemplate.rollback(conn);
+		}
+		JDBCTemplate.close(conn);
+		
+		return result;
+	}
+
 }
--- javaWorkspace/displayFlex/src/main/java/displayFlex/member/MemberVo.java
+++ javaWorkspace/displayFlex/src/main/java/displayFlex/member/MemberVo.java
@@ -4,6 +4,7 @@ public class MemberVo {
 	
 	public String memberNo;
 	public String memberId;
+	public String memberName;
 	public String memberPwd;
 	public String gradeNo;
 	public String adminYn;
@@ -15,7 +16,6 @@ public class MemberVo {
 	public String privilegedYn;
 	public String registerNo;
 	public String gradeScore;
-	
 	public String getMemberNo() {
 		return memberNo;
 	}
@@ -27,6 +27,12 @@ public class MemberVo {
 	}
 	public void setMemberId(String memberId) {
 		this.memberId = memberId;
+	}
+	public String getMemberName() {
+		return memberName;
+	}
+	public void setMemberName(String memberName) {
+		this.memberName = memberName;
 	}
 	public String getMemberPwd() {
 		return memberPwd;
@@ -94,12 +100,13 @@ public class MemberVo {
 	public void setGradeScore(String gradeScore) {
 		this.gradeScore = gradeScore;
 	}
-	public MemberVo(String memberNo, String memberId, String memberPwd, String gradeNo, String adminYn,
-			String memberNick, String memberEmail, String memberPhoneNum, String enrolldate, String deleteYn,
-			String privilegedYn, String registerNo, String gradeScore) {
+	public MemberVo(String memberNo, String memberId, String memberName, String memberPwd, String gradeNo,
+			String adminYn, String memberNick, String memberEmail, String memberPhoneNum, String enrolldate,
+			String deleteYn, String privilegedYn, String registerNo, String gradeScore) {
 		super();
 		this.memberNo = memberNo;
 		this.memberId = memberId;
+		this.memberName = memberName;
 		this.memberPwd = memberPwd;
 		this.gradeNo = gradeNo;
 		this.adminYn = adminYn;
@@ -118,11 +125,11 @@ public class MemberVo {
 	}
 	@Override
 	public String toString() {
-		return "MemberVo [memberNo=" + memberNo + ", memberId=" + memberId + ", memberPwd=" + memberPwd + ", gradeNo="
-				+ gradeNo + ", adminYn=" + adminYn + ", memberNick=" + memberNick + ", memberEmail=" + memberEmail
-				+ ", memberPhoneNum=" + memberPhoneNum + ", enrolldate=" + enrolldate + ", deleteYn=" + deleteYn
-				+ ", privilegedYn=" + privilegedYn + ", registerNo=" + registerNo + ", gradeScore=" + gradeScore + "]";
+		return "MemberVo [memberNo=" + memberNo + ", memberId=" + memberId + ", memberName=" + memberName
+				+ ", memberPwd=" + memberPwd + ", gradeNo=" + gradeNo + ", adminYn=" + adminYn + ", memberNick="
+				+ memberNick + ", memberEmail=" + memberEmail + ", memberPhoneNum=" + memberPhoneNum + ", enrolldate="
+				+ enrolldate + ", deleteYn=" + deleteYn + ", privilegedYn=" + privilegedYn + ", registerNo="
+				+ registerNo + ", gradeScore=" + gradeScore + "]";
 	}
-	
 	
 }
